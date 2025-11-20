6a6278c24165ef1be3938c4e3da9836ff2a94f87
james.yu <james.yu@hytechc.com>
2025-11-20 18:50:52 +0800

    docker

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -26,7 +26,7 @@ public class HelloController {
     public Map<String, Object> hello() {
         Map<String, Object> response = new HashMap<>();
         response.put("status", "success");
-        response.put("message", "Hello from Demo App!");
+        response.put("message", "bulid with parameters!");
         response.put("timestamp", getCurrentTime());
         response.put("version", "2.0.0");
         response.put("environment", "Kubernetes");
